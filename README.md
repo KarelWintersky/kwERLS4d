30546f8618722880ea4e65f067d2903779e61261
Karel Wintersky <karel.wintersky@gmail.com>
2015-12-24 01:51:53 +0300

    0.2.0 build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kwERLS4d
-E.R.L.S. 4th Edition
+E.R.L.S. 4th Edition version 0.2.0
+
+[Последняя редакция](https://github.com/KarelWintersky/kwERLS4d/blob/master/html/erls4d.md)
---
+++ sources.js
@@ -0,0 +1,55 @@
+module.exports = {
+    srcList : [
+        // introduction
+        'source/0-1 - introduction.md',
+        'source/0-2 - toc.md',            // оглавление
+        'source/0-3 - crm.md',            // суть механики разрешения конфликтов
+
+        // 1 часть: параметры
+        'source/1-stats/1-1 - mainstats.md',
+        'source/1-stats/1-2 - addstats.md',
+        'source/1-stats/1-3 - passivebonus.md',
+        'source/1-stats/1-4 - calculatedstats.md',
+
+        // 2 часть: навыки
+        'source/2-skills/2-1 - intro.md',
+        'source/2-skills/2-2 - levels.md',
+        'source/2-skills/2-3 - s&s.md',
+        'source/2-skills/2-4 - novice.md',
+        'source/2-skills/2-5 - outro.md',
+
+        // 3 часть: 3-chargen
+        'source/3-chargen/3-1 - intro.md',
+        'source/3-chargen/3-2 - chargen.md',
+        'source/3-chargen/3-3 - health.md',
+        'source/3-chargen/3-4 - exping.md',
+        'source/3-chargen/3-5 - crm_again.md',
+        'source/3-chargen/3-6 - outro.md',
+
+        // 4 часть: 4-skills_info
+        'source/4-skills_info/4-1 - intro.md',
+        'source/4-skills_info/4-2 - weapons.md',
+        'source/4-skills_info/4-3 - transport.md',
+        'source/4-skills_info/4-4 - armour.md',
+        'source/4-skills_info/4-5 - skills_str.md',
+        'source/4-skills_info/4-6 - skills_dex.md',
+        'source/4-skills_info/4-7 - skills_int.md',
+        'source/4-skills_info/4-8 - skills_wll.md',
+        'source/4-skills_info/4-9 - skills_other.md',
+        'source/4-skills_info/4-10 - body_enhance.md',
+        'source/4-skills_info/4-11 - outro.md',
+
+        // 5 паранормалка
+
+        // 6 дополнения
+        'source/5-addons/5-1 - intro.md',
+        'source/5-addons/5-2 - youngsters.md',
+        'source/5-addons/5-3 - disabilities.md',
+        'source/5-addons/5-4 - conversion.md',
+        'source/5-addons/5-5 - outro.md',
+
+        // final
+        'source/glossary.md' // глоссарий
+
+    ]
+};
